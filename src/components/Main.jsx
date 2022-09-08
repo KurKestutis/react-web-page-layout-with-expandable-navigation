c1049774864cacbe7c011da2be9b9b4a8d8d7fd0
Kestutis Kurienius <kurkestutis@gmail.com>
2022-09-08 16:39:43 +0300

    Main and Nav components added.

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod maxime
+        eum quas illum expedita saepe consectetur fugit aliquam ex iusto
+        deserunt adipisci modi velit veniam qui et quae quisquam, laudantium
+        recusandae, commodi nemo tenetur ipsum? Officiis beatae, error obcaecati
+        illum rerum quas, molestias suscipit ratione provident, aliquam minima
+        praesentium?
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import "./Navigation.scss";
+
+const Navigation = () => {
+  return (
+    <nav className="nav">
+      <button className="nav-btn">Expan</button>
+    </nav>
+  );
+};
+
+export default Navigation;
