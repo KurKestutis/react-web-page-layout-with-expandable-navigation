0b646361cfc31d6c1b8191ebdaa9a7f7b6dbeb75
Kestutis Kurienius <kurkestutis@gmail.com>
2022-09-09 12:08:30 +0300

    Nav expands and shrinks.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,31 @@
+import { useState, classNames } from "react";
 import "./Navigation.scss";
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const [expan, setExpan] = useState(false);
+
+  const expanShrink = (props) => {
+    console.log(`btn expanShrink pushed`);
+    expan ? setExpan(false) : setExpan(true);
+  };
+
   return (
-    <nav className="nav">
-      <button className="nav-btn">Expan</button>
+    <nav
+      className={
+        expan ? (classNames = "nav nav--expanded") : (classNames = "nav")
+      }
+    >
+      <button
+        type="button"
+        className={`'nav-btn', ${
+          expan
+            ? (classNames = "nav-btn nav--expanded-btn")
+            : (classNames = "nav-btn")
+        }`}
+        onClick={expanShrink}
+      >
+        Expan Nav
+      </button>
     </nav>
   );
 };
