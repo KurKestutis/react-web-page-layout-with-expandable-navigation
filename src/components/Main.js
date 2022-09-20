55baedcd134cfc718716c674e942255d69837dfb
Kestutis Kurienius <kurkestutis@gmail.com>
2022-09-20 14:28:35 +0300

    Not Found page added and minor color change.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import "./Main.scss";
 import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
 import About from "./Pages/About";
+import NotFound from "./NotFound";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -14,6 +15,7 @@ const Main = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   );
