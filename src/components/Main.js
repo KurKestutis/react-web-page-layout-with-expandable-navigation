1a946dd56d3c25af02fdf4681962ac6ada007a95
Kestutis Kurienius <kurkestutis@gmail.com>
2022-09-20 13:41:18 +0300

    Changed BrowserRouter to HashRouter.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
 import About from "./Pages/About";
 
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const Main = () => {
   return (
