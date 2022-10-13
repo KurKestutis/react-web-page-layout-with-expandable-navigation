7fc114487cb04ee5f966ab62a0c174594fcb0960
Kestutis Kurienius <kurkestutis@gmail.com>
2022-10-13 16:30:34 +0300

    Added media queries, scss variables, some text & small fixes.

--- src/components/MainLayoutComponents/Header.js
+++ src/components/MainLayoutComponents/Header.js
@@ -3,7 +3,7 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Company Name</h1>
+      <h1>Kęstutis Kurienius</h1>
     </header>
   );
 };
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,15 @@
 import "./Home.scss";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>
+        Web project to practise React, states, components and to hold portfolio
+        projects. Please explore.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
